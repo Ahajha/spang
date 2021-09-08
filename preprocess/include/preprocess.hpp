86985e78895f5498e733df266c84df640512b7a3
Alex Trotta <ahajha@gmail.com>
2021-09-08 04:58:02 -0400

    fix indentation in preprocess to be consistent with other libraries

--- preprocess/include/preprocess.hpp
+++ preprocess/include/preprocess.hpp
@@ -10,65 +10,67 @@
 
 namespace spang
 {
-	/*!
-	Searches graphs for each vertex and edge label that occurs in at least min_freq graphs.
-	For each frequent label, includes a list of graphs that the label occurs in.
-	*/
-	std::pair<std::map<vertex_label_t, std::vector<graph_id_t>>,
-	          std::map<edge_label_t, std::vector<graph_id_t>>>
-	  find_frequent_label_occurrences(const std::vector<parsed_input_graph_t>& graphs,
-	    std::size_t min_freq);
-	
-	/*!
-	An occurrence count must be large enough to potentially include all graphs. A graph_id_t
-	would also be sufficient here.
-	*/
-	using occurrence_count = std::size_t;
-	
-	/*!
-	Searches graphs for each vertex and edge label that occurs in at least min_freq graphs.
-	For each frequent label, includes the number of graphs the label occurs in.
-	*/
-	std::pair<std::map<vertex_label_t, occurrence_count>,
-	          std::map<edge_label_t, occurrence_count>>
-	  find_frequent_labels(const std::vector<parsed_input_graph_t>& graphs,
-	    std::size_t min_freq);
-	
-	/*!
-	Compact graph representation. Micro-optimization to
-	avoid cache misses while observing a single graph.
-	*/
-	struct compact_graph_t
+
+/*!
+Searches graphs for each vertex and edge label that occurs in at least min_freq graphs.
+For each frequent label, includes a list of graphs that the label occurs in.
+*/
+std::pair<std::map<vertex_label_t, std::vector<graph_id_t>>,
+          std::map<edge_label_t, std::vector<graph_id_t>>>
+  find_frequent_label_occurrences(const std::vector<parsed_input_graph_t>& graphs,
+    std::size_t min_freq);
+
+/*!
+An occurrence count must be large enough to potentially include all graphs. A graph_id_t
+would also be sufficient here.
+*/
+using occurrence_count = std::size_t;
+
+/*!
+Searches graphs for each vertex and edge label that occurs in at least min_freq graphs.
+For each frequent label, includes the number of graphs the label occurs in.
+*/
+std::pair<std::map<vertex_label_t, occurrence_count>,
+          std::map<edge_label_t, occurrence_count>>
+  find_frequent_labels(const std::vector<parsed_input_graph_t>& graphs,
+    std::size_t min_freq);
+
+/*!
+Compact graph representation. Micro-optimization to
+avoid cache misses while observing a single graph.
+*/
+struct compact_graph_t
+{
+	struct compact_vertex_t
 	{
-		struct compact_vertex_t
-		{
-			vertex_label_t label;
-			vertex_id_t id;
-			std::span<edge_t> edges;
-		};
-		
-		graph_id_t id;
-		int n_edges = 0;
-		std::vector<compact_vertex_t> vertices;
-		
-		//! Compacts a given graph.
-		compact_graph_t(const graph_t& source);
-		
-		private:
-		
-		// Contains all edges in a single allocation, vertices reference this via std::spans.
-		std::unique_ptr<edge_t[]> edges;
+		vertex_label_t label;
+		vertex_id_t id;
+		std::span<edge_t> edges;
 	};
 	
-	/*!
-	Prunes infrequent edges and vertices and converts from a edge list
-	to an adjacency list format.
-	(Note: the values in frequency are ignored, only the keys are used.
-	A std::set would be used, but this is intended to be used with the functions above.)
-	*/
-	template<class value_t>
-	std::vector<compact_graph_t> prune_infrequent_labels(
-	  const std::vector<parsed_input_graph_t>& graphs,
-	  std::map<vertex_label_t, value_t> freq_vertex_labels,
-	  std::map<edge_label_t, value_t> freq_edge_labels);
+	graph_id_t id;
+	int n_edges = 0;
+	std::vector<compact_vertex_t> vertices;
+	
+	//! Compacts a given graph.
+	compact_graph_t(const graph_t& source);
+	
+	private:
+	
+	// Contains all edges in a single allocation, vertices reference this via std::spans.
+	std::unique_ptr<edge_t[]> edges;
+};
+
+/*!
+Prunes infrequent edges and vertices and converts from a edge list
+to an adjacency list format.
+(Note: the values in frequency are ignored, only the keys are used.
+A std::set would be used, but this is intended to be used with the functions above.)
+*/
+template<class value_t>
+std::vector<compact_graph_t> prune_infrequent_labels(
+	const std::vector<parsed_input_graph_t>& graphs,
+	std::map<vertex_label_t, value_t> freq_vertex_labels,
+	std::map<edge_label_t, value_t> freq_edge_labels);
+
 }
--- preprocess/src/preprocess.cpp
+++ preprocess/src/preprocess.cpp
@@ -4,127 +4,129 @@
 
 namespace spang
 {
-	// functor for determining if an item in a map is infrequent. Used as a convenience
-	// to avoid code duplication.
-	struct prune_infrequent
-	{
-		std::size_t min_freq;
-		
-		template<class label_t>
-		bool operator()(const std::pair<const label_t, std::vector<graph_id_t>>& kv_pair) const
-		{
-			return kv_pair.second.size() < min_freq;
-		}
-		
-		template<class label_t>
-		bool operator()(const std::pair<const label_t, occurrence_count>& kv_pair) const
-		{
-			return kv_pair.second < min_freq;
-		}
-	};
+
+// functor for determining if an item in a map is infrequent. Used as a convenience
+// to avoid code duplication.
+struct prune_infrequent
+{
+	std::size_t min_freq;
 	
-	std::pair<std::map<vertex_label_t, std::vector<graph_id_t>>,
-	          std::map<edge_label_t, std::vector<graph_id_t>>>
-	  find_frequent_label_occurrences(const std::vector<parsed_input_graph_t>& graphs,
-	    std::size_t min_freq)
+	template<class label_t>
+	bool operator()(const std::pair<const label_t, std::vector<graph_id_t>>& kv_pair) const
 	{
-		// Start by creating maps containing all labels, then prune infrequent ones later.
-		std::map<vertex_label_t, std::vector<graph_id_t>> freq_vertex_labels;
-		std::map<edge_label_t, std::vector<graph_id_t>> freq_edge_labels;
-		
-		for (const auto& graph : graphs)
-		{
-			// Temporary sets are necessary to ensure each label is counted at most
-			// once per graph.
-			{
-				std::set<vertex_label_t> vertex_labels;
-				for (const auto& vertex : graph.vertices)
-				{
-					if (vertex_labels.insert(vertex.label).second)
-					{
-						// Only on the first occurence place into freq_vertex_labels.
-						freq_vertex_labels[vertex.label].push_back(graph.id);
-					}
-				}
-			}
-			
-			{
-				std::set<edge_label_t> edge_labels;
-				for (const auto& edge : graph.edges)
-				{
-					if (edge_labels.insert(edge.label).second)
-					{
-						// Only on the first insertion place into freq_edge_labels.
-						freq_edge_labels[edge.label].push_back(graph.id);
-					}
-				}
-			}
-		}
-		
-		// Prune infrequent labels.
-		std::erase_if(freq_vertex_labels, prune_infrequent{min_freq});
-		std::erase_if(freq_edge_labels, prune_infrequent{min_freq});
-		
-		return { freq_vertex_labels, freq_edge_labels };
+		return kv_pair.second.size() < min_freq;
 	}
 	
-	std::pair<std::map<vertex_label_t, occurrence_count>,
-	          std::map<edge_label_t, occurrence_count>>
-	  find_frequent_labels(const std::vector<parsed_input_graph_t>& graphs,
-	    std::size_t min_freq)
+	template<class label_t>
+	bool operator()(const std::pair<const label_t, occurrence_count>& kv_pair) const
 	{
-		// Start by creating maps containing all labels, then prune infrequent ones later.
-		std::map<vertex_label_t, occurrence_count> freq_vertex_labels;
-		std::map<edge_label_t, occurrence_count> freq_edge_labels;
-		
-		for (const auto& graph : graphs)
+		return kv_pair.second < min_freq;
+	}
+};
+
+std::pair<std::map<vertex_label_t, std::vector<graph_id_t>>,
+          std::map<edge_label_t, std::vector<graph_id_t>>>
+  find_frequent_label_occurrences(const std::vector<parsed_input_graph_t>& graphs,
+    std::size_t min_freq)
+{
+	// Start by creating maps containing all labels, then prune infrequent ones later.
+	std::map<vertex_label_t, std::vector<graph_id_t>> freq_vertex_labels;
+	std::map<edge_label_t, std::vector<graph_id_t>> freq_edge_labels;
+	
+	for (const auto& graph : graphs)
+	{
+		// Temporary sets are necessary to ensure each label is counted at most
+		// once per graph.
 		{
-			// Temporary sets are necessary to ensure each label is counted at most
-			// once per graph.
+			std::set<vertex_label_t> vertex_labels;
+			for (const auto& vertex : graph.vertices)
 			{
-				std::set<vertex_label_t> vertex_labels;
-				for (const auto& vertex : graph.vertices)
+				if (vertex_labels.insert(vertex.label).second)
 				{
-					if (vertex_labels.insert(vertex.label).second)
-					{
-						// Only count the first occurence.
-						++freq_vertex_labels[vertex.label];
-					}
-				}
-			}
-			
-			{
-				std::set<edge_label_t> edge_labels;
-				for (const auto& edge : graph.edges)
-				{
-					if (edge_labels.insert(edge.label).second)
-					{
-						// Only count the first occurence.
-						++freq_edge_labels[edge.label];
-					}
+					// Only on the first occurence place into freq_vertex_labels.
+					freq_vertex_labels[vertex.label].push_back(graph.id);
 				}
 			}
 		}
 		
-		// Prune infrequent labels.
-		std::erase_if(freq_vertex_labels, prune_infrequent{min_freq});
-		std::erase_if(freq_edge_labels, prune_infrequent{min_freq});
-		
-		return { freq_vertex_labels, freq_edge_labels };
+		{
+			std::set<edge_label_t> edge_labels;
+			for (const auto& edge : graph.edges)
+			{
+				if (edge_labels.insert(edge.label).second)
+				{
+					// Only on the first insertion place into freq_edge_labels.
+					freq_edge_labels[edge.label].push_back(graph.id);
+				}
+			}
+		}
 	}
 	
-	compact_graph_t::compact_graph_t(const graph_t& source) : id(source.id),
-		n_edges(source.n_edges), edges(std::make_unique<edge_t[]>(2 * source.n_edges))
+	// Prune infrequent labels.
+	std::erase_if(freq_vertex_labels, prune_infrequent{min_freq});
+	std::erase_if(freq_edge_labels, prune_infrequent{min_freq});
+	
+	return { freq_vertex_labels, freq_edge_labels };
+}
+
+std::pair<std::map<vertex_label_t, occurrence_count>,
+          std::map<edge_label_t, occurrence_count>>
+  find_frequent_labels(const std::vector<parsed_input_graph_t>& graphs,
+    std::size_t min_freq)
+{
+	// Start by creating maps containing all labels, then prune infrequent ones later.
+	std::map<vertex_label_t, occurrence_count> freq_vertex_labels;
+	std::map<edge_label_t, occurrence_count> freq_edge_labels;
+	
+	for (const auto& graph : graphs)
 	{
-		for (auto iter = edges.get(); const auto& src_vert : source.vertices)
+		// Temporary sets are necessary to ensure each label is counted at most
+		// once per graph.
 		{
-			// Construct the vertex
-			vertices.emplace_back(src_vert.label, src_vert.id,
-				std::span{iter, src_vert.edges.size()});
-			
-			// Copy the edges from the source graph into this one, and have the iterator
-			// point to the next edge after to prepare for the next iteration.
-			iter = std::ranges::copy(src_vert.edges, iter).out;
+			std::set<vertex_label_t> vertex_labels;
+			for (const auto& vertex : graph.vertices)
+			{
+				if (vertex_labels.insert(vertex.label).second)
+				{
+					// Only count the first occurence.
+					++freq_vertex_labels[vertex.label];
+				}
+			}
+		}
+		
+		{
+			std::set<edge_label_t> edge_labels;
+			for (const auto& edge : graph.edges)
+			{
+				if (edge_labels.insert(edge.label).second)
+				{
+					// Only count the first occurence.
+					++freq_edge_labels[edge.label];
+				}
+			}
 		}
 	}
+	
+	// Prune infrequent labels.
+	std::erase_if(freq_vertex_labels, prune_infrequent{min_freq});
+	std::erase_if(freq_edge_labels, prune_infrequent{min_freq});
+	
+	return { freq_vertex_labels, freq_edge_labels };
+}
+
+compact_graph_t::compact_graph_t(const graph_t& source) : id(source.id),
+	n_edges(source.n_edges), edges(std::make_unique<edge_t[]>(2 * source.n_edges))
+{
+	for (auto iter = edges.get(); const auto& src_vert : source.vertices)
+	{
+		// Construct the vertex
+		vertices.emplace_back(src_vert.label, src_vert.id,
+			std::span{iter, src_vert.edges.size()});
+		
+		// Copy the edges from the source graph into this one, and have the iterator
+		// point to the next edge after to prepare for the next iteration.
+		iter = std::ranges::copy(src_vert.edges, iter).out;
+	}
+}
+
 }

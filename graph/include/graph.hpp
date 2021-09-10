a963345d28c254f6ed2b91700dce779920c471ce
Alex Trotta <ahajha@gmail.com>
2021-09-10 06:47:18 -0400

    add graph_t::add_edge()

--- graph/include/graph.hpp
+++ graph/include/graph.hpp
@@ -28,8 +28,17 @@ struct vertex_t
 struct graph_t
 {
 	graph_id_t id;
-	int n_edges;
+	int n_edges = 0;
 	std::vector<vertex_t> vertices;
+	
+	//! Adds an undirected edge to the graph.
+	void add_edge(vertex_id_t from_id, edge_label_t edge_label, vertex_id_t to_id)
+	{
+		const auto edge_id = static_cast<edge_id_t>(n_edges);
+		vertices[to_id].edges.emplace_back(to_id, from_id, edge_label, edge_id);
+		vertices[from_id].edges.emplace_back(from_id, to_id, edge_label, edge_id);
+		++n_edges;
+	}
 };
 
 }
